ee149dce6b22509a9673fd519aa8400b20157616
vegajdr <vegajdr@users.noreply.github.com>
2020-04-19 22:38:08 -0400

    Initial Commit

---
+++ lib/activerecord/query_object.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'activerecord/query_object/version'
+
+module ActiveRecord
+  module QueryObject
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
